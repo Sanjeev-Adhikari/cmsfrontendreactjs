d0b37993db97447ec904b5d3440e3545c2f33209
Sanjeev Adhikari <sanjeevadhikari75@gmail.com>
2024-07-24 07:16:20 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <h1 style={{color: "blue" }}>Hello World</h1>
+      <h2 style={{color: "red" }}>This is me</h2>
+      <h3>{1+2+5+6}</h3>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default App
